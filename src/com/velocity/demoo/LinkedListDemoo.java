b53c1bb7a9b30a8548627d594955916e99a0c70d
nehabhosle <nehabhosle215@gmail.com>
2022-12-26 22:45:12 +0530

    second commit : added code for linkedlist

---
+++ src/com/velocity/demoo/LinkedListDemoo.java
@@ -0,0 +1,15 @@
+package com.velocity.demoo;
+
+import java.util.LinkedList;
+
+public class LinkedListDemoo {
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		
+		System.out.println(list);
+	}
+
+}
