2d191fa3c4c2b47435d8d132be80e00bc1498d19
nehabhosle <nehabhosle215@gmail.com>
2022-12-26 22:30:16 +0530

    first commit : added arraylist demo code

---
+++ src/com/velocity/demoo/ArrayListDemoo.java
@@ -0,0 +1,15 @@
+package com.velocity.demoo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemoo {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Shyam");
+		list.add("Mohan");
+		list.add("vinod");
+		
+		System.out.println(list);
+	}
+
+}
